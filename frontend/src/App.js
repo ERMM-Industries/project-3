fb711756b368cb3efb37ecce2c41a747549cb8b5
MauricioGtrz <mauriciogtrz@g.ucla.edu>
2020-09-14 13:37:07 -0700

    Created basic react app. Created Pages and Components directory and modified App.js to route appropiately.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return <div>Initial app</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/register" exact component={RegisterPage} />
+        <Route path="/login" exact component={LoginPage} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
