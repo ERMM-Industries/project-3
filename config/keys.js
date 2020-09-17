e6d9eb88614f06e865bc965c254ad4f9c0717c63
MauricioGtrz <mauriciogtrz@g.ucla.edu>
2020-09-17 09:15:22 -0700

    Changed keys file to connect my cloudniray, atlas database and email link

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,8 @@
-if(process.env.NODE_ENV==='production'){
-    module.exports = require('./prod')
-}else{
-    module.exports = require('./dev')
-}
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://Mauricio:Mauricio@weatcluster.hwx2c.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  JWT_SECRET: "jabfkb",
+  SENDGRID_API:
+    "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",
+  EMAIL: "http://localhost:3000",
+};
