a00e39cf6f36366c5ac439bc0bf45e8a49e181a8
Mauricio Gutierrez <mauriciogtrz@g.ucla.edu>
2020-09-18 08:26:50 -0700

    Added copyright logo component

--- client/src/pages/screens/CreatePost.js
+++ client/src/pages/screens/CreatePost.js
@@ -1,162 +1,19 @@
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import { fade } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
 import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import MenuLists from '../../components/MenuList/index'
-import { mainListItems, secondaryListItems } from '../../components/listItems/listItems';
 import AdGrid from '../../components/AdGrid/AdGrid';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
 import InputRecipe2 from '../../components/InputRecipe2/index'
-//import LogoutButton from '../../components/LogoutButton/logout-button';
-//import { MenuList } from '@material-ui/core';
-//import { Col, Row, Container } from "../../components/Grid";
-//import { Input, TextArea, FormBtn } from "../../components/Form/index";
-//import Jumbotron from "../../components/Jumbotron";
 //everything above is for styling (fixed file routes for components)
-import React, { useState, useEffect } from "react";
-import M from "materialize-css";
-import { useHistory } from "react-router-dom";
-
+import React from "react";
 import NavBar from "../../components/Navigation/index"
-
-const PreviousCretePost = () => {
-  const history = useHistory();
-  const [title, setTitle] = useState("");
-  const [ingredients, setIngredients] = useState(""); //added set ingredients
-  const [instructions, setInstructions] = useState(""); //added set instructions
-  const [image, setImage] = useState("");
-  const [url, setUrl] = useState("");
-  useEffect(() => {
-    if (url) {
-      fetch("/createpost", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + localStorage.getItem("jwt"),
-        },
-        body: JSON.stringify({
-          title,
-          ingredients, //added ingredients
-          instructions, //added instructions
-          pic: url,
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.error) {
-            M.toast({ html: data.error, classes: "#c62828 red darken-3" });
-          } else {
-            M.toast({
-              html: "Created post Successfully",
-              classes: "#43a047 green darken-1",
-            });
-            history.push("/");
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [url]);
-
-  const postDetails = () => {
-    const data = new FormData();
-    data.append("file", image);
-    data.append("upload_preset", "weat-project"); // added database preset
-    data.append("cloud_name", "dgav9dwqa");
-    //api cloudinary call
-    fetch("https://api.cloudinary.com/v1_1/dgav9dwqa/image/upload", {
-      method: "post",
-      body: data,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setUrl(data.url);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  return (
-    <div
-      className="card input-filed"
-      style={{
-        margin: "30px auto",
-        maxWidth: "500px",
-        padding: "20px",
-        textAlign: "center",
-      }}
-    >
-      <input
-        type="text"
-        placeholder="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input //added ingredients input
-        type="text"
-        placeholder="ingredients"
-        value={ingredients}
-        onChange={(e) => setIngredients(e.target.value)}
-      />
-      <input //added instructions input
-        type="text"
-        placeholder="instructions"
-        value={instructions}
-        onChange={(e) => setInstructions(e.target.value)}
-      />
-      <div className="file-field input-field">
-        <div className="btn #64b5f6 blue darken-1">
-          <span>Uplaod Image</span>
-          <input type="file" onChange={(e) => setImage(e.target.files[0])} />
-        </div>
-        <div className="file-path-wrapper">
-          <input className="file-path validate" type="text" />
-        </div>
-      </div>
-      <button
-        className="btn waves-effect waves-light #64b5f6 blue darken-1"
-        onClick={() => postDetails()}
-      >
-        Submit post
-      </button>
-    </div>
-  );
-};
-
-//export default PreviousCretePost;
+import Copyright from "../../components/Copyright/index"
 
 
 //styling stuff
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Weat
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const drawerWidth = 240;
 
@@ -189,35 +46,6 @@ const useStyles = makeStyles((theme) => ({
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-  },
   appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
@@ -237,108 +65,15 @@ const useStyles = makeStyles((theme) => ({
   fixedHeight: {
     height: 240,
   },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   inputRoot: {
     color: 'inherit',
   },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-  
 }));
 
 //styling stuff
 
 const NewCretePost = () => {
-  const history = useHistory();
-  const [title, setTitle] = useState("");
-  const [ingredients, setIngredients] = useState(""); //added set ingredients
-  const [instructions, setInstructions] = useState(""); //added set instructions
-  const [image, setImage] = useState("");
-  const [url, setUrl] = useState("");
-  useEffect(() => {
-    if (url) {
-      fetch("/createpost", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + localStorage.getItem("jwt"),
-        },
-        body: JSON.stringify({
-          title,
-          ingredients, //added ingredients
-          instructions, //added instructions
-          pic: url,
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.error) {
-            M.toast({ html: data.error, classes: "#c62828 red darken-3" });
-          } else {
-            M.toast({
-              html: "Created post Successfully",
-              classes: "#43a047 green darken-1",
-            });
-            history.push("/");
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [url]);
-
-  const postDetails = () => {
-    const data = new FormData();
-    data.append("file", image);
-    data.append("upload_preset", "weat-project"); // added database preset
-    data.append("cloud_name", "dgav9dwqa");
-    //api cloudinary call
-    fetch("https://api.cloudinary.com/v1_1/dgav9dwqa/image/upload", {
-      method: "post",
-      body: data,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setUrl(data.url);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  //logic is above
-
+  
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
   const handleDrawerOpen = () => {
